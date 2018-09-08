d8bb6ad2b453553890fada6db611ce0693d9c59b
Dominic S <git@dominic86.de>
2018-09-09 01:19:05 +0200

    added some filters

--- hostingde/helpers/filters.py
+++ hostingde/helpers/filters.py
@@ -5,11 +5,28 @@ def getOrFilter(subFilters):
         'subFilter': subFilters
     }
 
+
+def getOrFilters(**kwArgs):
+    return getOrFilter(getFilters(**kwArgs))
+
+
 def getAndFilter(subFilters):
     return {
         'subFilterConnective': 'AND', 
         'subFilter': subFilters
     }
+
+
+def getAndFilters(**kwArgs):
+    return getAndFilter(getFilters(**kwArgs))
+
+
+def getFilters(**kwArgs):
+    filters = []
+    for key, value in kwArgs.items():
+        filters.append(getFilter(key, value))
+    return filters
+
 
 def getFilter(field, value, relation=None):
     if relation:
@@ -22,3 +39,19 @@ def getFilter(field, value, relation=None):
         'field': field, 
         'value': value
     }
+
+
+def getRecordFilter(recordName, recordType=None, recordContent=None):
+    recordFilters = [getFilter("RecordName", recordName)]
+    if recordType: 
+        recordFilters.append(getFilter("RecordType", recordType))
+    if recordContent:
+        recordFilters.append(getFilter("RecordContent", recordContent))
+    return getAndFilter(recordFilters)
+
+
+def getZoneDomainListFilter(domains):
+    filters = []
+    for domain in domains:
+        filters.append(getFilter("ZoneNameUnicode", domain))
+    return getOrFilter(filters)
